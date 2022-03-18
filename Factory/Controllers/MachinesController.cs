43487c4eca90d4718701ffa0c3961ccecb12f629
Liam Eller <liamthelastson@gmail.com>
2022-03-18 09:21:40 -0700

    Rename and add Index route to MachinesController and corresponding .cshtml file to Views

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Factory.Models;
+using System.Linq;
+using System;
+
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Machine> queryModel = _db.Machines.OrderBy(machine => machine.Name).ToList();
+      return View(queryModel);
+    }
+  }
+}
