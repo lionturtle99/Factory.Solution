c56c3b3c008faaef27197d39d27f6cd5d962f07a
Liam Eller <liamthelastson@gmail.com>
2022-03-18 16:10:43 -0700

    Add lists of all machines and engineers to home page, and additional styling

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,27 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Factory.Models;
+using System.Linq;
+using System;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
     {
+      ViewBag.Machines = _db.Machines.OrderBy(machine => machine.Name).ToList();
+      ViewBag.Engineers = _db.Engineers.OrderBy(engineer => engineer.Name).ToList();
       return View();
     }
 
