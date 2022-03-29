79aad759c4d3d4fde94496ce79f2341c8098ccfc
Liam Eller <liamthelastson@gmail.com>
2022-03-28 20:33:25 -0700

    Add migrattion to reflect current database structure

---
+++ Factory/Migrations/20220329030205_DateEmployed.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Factory.Migrations
+{
+    public partial class DateEmployed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Engineers",
+                newName: "DateEmployed");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateEmployed",
+                table: "Engineers",
+                newName: "DateTime");
+        }
+    }
+}
